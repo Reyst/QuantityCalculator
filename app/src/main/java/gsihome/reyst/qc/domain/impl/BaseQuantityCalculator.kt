ba5e35022812665c3e5edbc1dec56a9ec10f5db1
Reyst <oleksenko.pm@gmail.com>
2019-07-20 11:26:13 +0300

    update versions

--- app/src/main/java/gsihome/reyst/qc/domain/impl/BaseQuantityCalculator.kt
+++ app/src/main/java/gsihome/reyst/qc/domain/impl/BaseQuantityCalculator.kt
@@ -5,7 +5,7 @@ import gsihome.reyst.qc.domain.QuantityCalculator
 abstract class BaseQuantityCalculator : QuantityCalculator {
 
     protected fun isValueIncorrect(result: Double) = result < 0 ||
-            result == Double.POSITIVE_INFINITY ||
-            result == Double.NEGATIVE_INFINITY ||
-            result == Double.NaN
+        result == Double.POSITIVE_INFINITY ||
+        result == Double.NEGATIVE_INFINITY ||
+        result.isNaN()
 }
